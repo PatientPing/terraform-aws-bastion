6d95f98d8aaac707152da48053452edf01e22705
Eric Yablonowitz <eyablonowitz@patientping.com>
2019-07-30 12:07:05 -0400

    remove unnecesary interpolations

--- main.tf
+++ main.tf
@@ -1,44 +1,44 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.bucket_name}"
+  bucket = var.bucket_name
   acl    = "bucket-owner-full-control"
 
-  force_destroy = "${var.bucket_force_destroy}"
+  force_destroy = var.bucket_force_destroy
 
   versioning {
-    enabled = "${var.bucket_versioning}"
+    enabled = var.bucket_versioning
   }
 
   lifecycle_rule {
     id      = "log"
-    enabled = "${var.log_auto_clean}"
+    enabled = var.log_auto_clean
 
     prefix = "logs/"
 
     tags = {
       "rule"      = "log"
-      "autoclean" = "${var.log_auto_clean}"
+      "autoclean" = var.log_auto_clean
     }
 
     transition {
-      days          = "${var.log_standard_ia_days}"
+      days          = var.log_standard_ia_days
       storage_class = "STANDARD_IA"
     }
 
     transition {
-      days          = "${var.log_glacier_days}"
+      days          = var.log_glacier_days
       storage_class = "GLACIER"
     }
 
     expiration {
-      days = "${var.log_expiry_days}"
+      days = var.log_expiry_days
     }
   }
 
-  tags = "${merge(var.tags)}"
+  tags = merge(var.tags)
 }
 
 resource "aws_s3_bucket_object" "bucket_public_keys_readme" {
-  bucket  = "${aws_s3_bucket.bucket.id}"
+  bucket  = aws_s3_bucket.bucket.id
   key     = "public-keys/README.txt"
   content = "Drop here the ssh public keys of the instances you want to control"
 }
@@ -46,52 +46,52 @@ resource "aws_s3_bucket_object" "bucket_public_keys_readme" {
 resource "aws_security_group" "bastion_host_security_group" {
   description = "Enable SSH access to the bastion host from external via SSH port"
   name_prefix = var.resource_name_prefix
-  vpc_id      = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
 
-  tags = "${merge(var.tags)}"
+  tags = merge(var.tags)
 }
 
 resource "aws_security_group_rule" "ingress_bastion" {
   description = "Incoming traffic to bastion"
   type        = "ingress"
-  from_port   = "${var.public_ssh_port}"
-  to_port     = "${var.public_ssh_port}"
+  from_port   = var.public_ssh_port
+  to_port     = var.public_ssh_port
   protocol    = "TCP"
-  cidr_blocks = "${concat(data.aws_subnet.subnets.*.cidr_block, var.cidrs)}"
+  cidr_blocks = concat(data.aws_subnet.subnets.*.cidr_block, var.cidrs)
 
-  security_group_id = "${aws_security_group.bastion_host_security_group.id}"
+  security_group_id = aws_security_group.bastion_host_security_group.id
 }
 
 resource "aws_security_group_rule" "egress_bastion" {
   description = "Outgoing traffic from bastion to instances"
-  count       = "${var.bastion_open_egress == true ? 1 : 0}"
+  count       = var.bastion_open_egress == true ? 1 : 0
   type        = "egress"
   from_port   = "0"
   to_port     = "65535"
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = "${aws_security_group.bastion_host_security_group.id}"
+  security_group_id = aws_security_group.bastion_host_security_group.id
 }
 
 resource "aws_security_group" "private_instances_security_group" {
   description = "Enable SSH access to the Private instances from the bastion via SSH port"
   name_prefix = "${var.resource_name_prefix}-private-instances"
-  vpc_id      = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
 
-  tags = "${merge(var.tags)}"
+  tags = merge(var.tags)
 }
 
 resource "aws_security_group_rule" "ingress_instances" {
   description = "Incoming traffic from bastion"
   type        = "ingress"
-  from_port   = "${var.public_ssh_port}"
-  to_port     = "${var.public_ssh_port}"
+  from_port   = var.public_ssh_port
+  to_port     = var.public_ssh_port
   protocol    = "TCP"
 
-  source_security_group_id = "${aws_security_group.bastion_host_security_group.id}"
+  source_security_group_id = aws_security_group.bastion_host_security_group.id
 
-  security_group_id = "${aws_security_group.private_instances_security_group.id}"
+  security_group_id = aws_security_group.private_instances_security_group.id
 }
 
 resource "aws_iam_role" "bastion_host_role" {
@@ -119,7 +119,7 @@ EOF
 }
 
 resource "aws_iam_role_policy" "bastion_host_role_policy" {
-  role = "${aws_iam_role.bastion_host_role.id}"
+  role = aws_iam_role.bastion_host_role.id
 
   policy = <<EOF
 {
@@ -154,32 +154,32 @@ EOF
 }
 
 resource "aws_route53_record" "bastion_record_name" {
-  name    = "${var.bastion_record_name}"
-  zone_id = "${var.hosted_zone_name != "" ? var.hosted_zone_name : "empty"}"
+  name    = var.bastion_record_name
+  zone_id = var.hosted_zone_name != "" ? var.hosted_zone_name : "empty"
   type    = "A"
-  count   = "${var.create_dns_record == true ? 1 : 0}"
+  count   = var.create_dns_record == true ? 1 : 0
 
   alias {
     evaluate_target_health = true
-    name                   = "${aws_lb.bastion_lb.dns_name}"
-    zone_id                = "${aws_lb.bastion_lb.zone_id}"
+    name                   = aws_lb.bastion_lb.dns_name
+    zone_id                = aws_lb.bastion_lb.zone_id
   }
 }
 
 resource "aws_lb" "bastion_lb" {
-  internal = "${var.is_lb_private}"
+  internal = var.is_lb_private
   name_prefix     = substr(var.resource_name_prefix, 0, 6)
 
-  subnets = "${var.elb_subnets}"
+  subnets = var.elb_subnets
 
   load_balancer_type = "network"
-  tags               = "${merge(var.tags)}"
+  tags               = merge(var.tags)
 }
 
 resource "aws_lb_target_group" "bastion_lb_target_group" {
-  port        = "${var.public_ssh_port}"
+  port        = var.public_ssh_port
   protocol    = "TCP"
-  vpc_id      = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
   target_type = "instance"
   deregistration_delay = 120
 
@@ -188,22 +188,22 @@ resource "aws_lb_target_group" "bastion_lb_target_group" {
     protocol = "TCP"
   }
 
-  tags = "${merge(var.tags)}"
+  tags = merge(var.tags)
 }
 
 resource "aws_lb_listener" "bastion_lb_listener_22" {
   default_action {
-    target_group_arn = "${aws_lb_target_group.bastion_lb_target_group.arn}"
+    target_group_arn = aws_lb_target_group.bastion_lb_target_group.arn
     type             = "forward"
   }
 
-  load_balancer_arn = "${aws_lb.bastion_lb.arn}"
-  port              = "${var.public_ssh_port}"
+  load_balancer_arn = aws_lb.bastion_lb.arn
+  port              = var.public_ssh_port
   protocol          = "TCP"
 }
 
 resource "aws_iam_instance_profile" "bastion_host_profile" {
-  role = "${aws_iam_role.bastion_host_role.name}"
+  role = aws_iam_role.bastion_host_role.name
   path = "/"
   name_prefix = var.resource_name_prefix
 }
@@ -212,13 +212,13 @@ resource "aws_launch_configuration" "bastion_launch_configuration" {
   name_prefix                 = var.resource_name_prefix
   image_id                    = var.bastion_ami_id == "" ? data.aws_ami.amazon-linux-2.id : var.bastion_ami_id
   instance_type               = "t2.nano"
-  associate_public_ip_address = "${var.associate_public_ip_address}"
+  associate_public_ip_address = var.associate_public_ip_address
   enable_monitoring           = true
-  iam_instance_profile        = "${aws_iam_instance_profile.bastion_host_profile.name}"
-  key_name                    = "${var.bastion_host_key_pair}"
+  iam_instance_profile        = aws_iam_instance_profile.bastion_host_profile.name
+  key_name                    = var.bastion_host_key_pair
 
   security_groups = [
-    "${aws_security_group.bastion_host_security_group.id}",
+    aws_security_group.bastion_host_security_group.id,
   ]
 
   user_data = templatefile("${path.module}/user_data.sh", {
@@ -235,19 +235,19 @@ resource "aws_launch_configuration" "bastion_launch_configuration" {
 
 resource "aws_autoscaling_group" "bastion_auto_scaling_group" {
   name                 = "ASG-${aws_launch_configuration.bastion_launch_configuration.name}"
-  launch_configuration = "${aws_launch_configuration.bastion_launch_configuration.name}"
-  max_size             = "${var.bastion_instance_count}"
-  min_size             = "${var.bastion_instance_count}"
-  desired_capacity     = "${var.bastion_instance_count}"
+  launch_configuration = aws_launch_configuration.bastion_launch_configuration.name
+  max_size             = var.bastion_instance_count
+  min_size             = var.bastion_instance_count
+  desired_capacity     = var.bastion_instance_count
 
-  vpc_zone_identifier = "${var.auto_scaling_group_subnets}"
+  vpc_zone_identifier = var.auto_scaling_group_subnets
 
   default_cooldown          = 180
   health_check_grace_period = 180
   health_check_type         = "EC2"
 
   target_group_arns = [
-    "${aws_lb_target_group.bastion_lb_target_group.arn}",
+    aws_lb_target_group.bastion_lb_target_group.arn,
   ]
 
   termination_policies = [
